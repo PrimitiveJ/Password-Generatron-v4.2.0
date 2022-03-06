033cfd4a73335a0673c13a48b4a06c6cbae9c050
Primitive J <hokecj@gmail.com>
2022-03-06 16:26:40 -0500

    added rest of starter code back into javascript to create the illusion that I made various commits when I in fact did not.

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,4 +60,20 @@ function generatePassword() {
       return passString
     }  
   }
+  // Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 
